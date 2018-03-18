a8d9ca32e889b901a422545476448bca34f33486
Stefano Falda <stefano.falda@gmail.com>
2018-03-18 23:05:13 +0100

    Updated typescript definition

--- lib/LocalizedStrings.d.ts
+++ lib/LocalizedStrings.d.ts
@@ -9,12 +9,48 @@ declare module 'localized-strings' {
     type GetInterfaceLanguageCallback = () => string;
 
     export interface LocalizedStringsMethods {
+        /**
+         * Can be used from ouside the class to force a particular language
+         * indipendently from the interface one
+         * @param language 
+         */
         setLanguage(language: string): void;
+
+        /**
+         *  The current language displayed (could differ from the interface language
+         *  if it has been forced manually and a matching translation has been found)
+         */
         getLanguage(): string;
+
+        /**
+         * The current interface language (could differ from the language displayed)
+         */
         getInterfaceLanguage(): string;
+
+        /**
+         * Format the passed string replacing the numbered placeholders
+         * i.e. I'd like some {0} and {1}, or just {0}
+         * Use example:
+         *   strings.formatString(strings.question, strings.bread, strings.butter)
+         */
         formatString<T extends Formatted>(str: string, ...values: Array<T | FormatObject<T>>): Array<string | T>;
+
+        /**
+         * Return an array containing the available languages passed as props in the constructor
+         */
         getAvailableLanguages(): string[];
+
+        /**
+         * Return a string with the passed key in a different language
+         * @param key 
+         * @param language 
+         */
         getString(key: string, language: string): string;
+
+        /**
+         * Replace the NamedLocalization object without reinstantiating the object
+         * @param props 
+         */
         setContent(props: any): void;
     }
 
@@ -25,6 +61,6 @@ declare module 'localized-strings' {
         new <T>(props: GlobalStrings<T>, getInterfaceLanguage?:GetInterfaceLanguageCallback): LocalizedStrings<T>;
     }
   
-    var LocalizedStrings: LocalizedStringsFactory;
+    const LocalizedStrings: LocalizedStringsFactory;
     export default LocalizedStrings;
   }
